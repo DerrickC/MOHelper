39d6e9f6251e2a2196992de6c6899d51a98dee5d
ChaoChung Shu <chaochungshu@ChaoChungdeMacBook-Pro.local>
2014-07-16 23:43:22 +0800

    增加方法

--- NSManagedObject+Helper.h
+++ NSManagedObject+Helper.h
@@ -11,7 +11,7 @@
 //
 // core data 的建立方式為 Xcode 預設的實作方式
 // 許多實作已經建立在 AppDelegate 裡面
-// Entity 有兩個 column: createdAt 和 updatedAt
+// Entity 需有兩個 column: createdAt 和 updatedAt
 
 
 #import <CoreData/CoreData.h>
@@ -34,6 +34,14 @@
 + (NSArray *)findBy:(NSString *)columnName value:(id)value;
 + (NSArray *)findBy:(NSString *)columnName value:(id)value limit:(NSUInteger)max;
 + (NSArray *)findBy:(NSString *)columnName value:(id)value limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending;
+
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values;
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values limit:(NSUInteger)max;
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending;
+
++ (NSArray *)findByPredicate:(NSPredicate *)predicate;
++ (NSArray *)findByPredicate:(NSPredicate *)predicate limit:(NSUInteger)max;
++ (NSArray *)findByPredicate:(NSPredicate *)predicate limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending;
 
 - (void)remove;
 
--- NSManagedObject+Helper.m
+++ NSManagedObject+Helper.m
@@ -144,18 +144,10 @@
 
 + (NSArray *)findBy:(NSString *)columnName value:(id)value limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending
 {
-    // *** create fetchRequest
-    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-    NSEntityDescription *entity = [NSEntityDescription
-                                   entityForName:SELF_CLASS_STRING
-                                   inManagedObjectContext:[self managedObjectContext]];
-    [fetchRequest setEntity:entity];
-    
     // *** predicate
+    NSPredicate *predicate;
     if (columnName) {
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self.%@ == %@", columnName, value];
-        [fetchRequest setPredicate:predicate];
-        NSLog(@"%@", predicate);
+        predicate = [NSPredicate predicateWithFormat:@"self.%@ == %@", columnName, value];
     }
     
     // *** sort conditions
@@ -170,25 +162,130 @@
         NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"createdAt" ascending:ascending];
         [sortDescriptors addObject:sort];
     }
-    [fetchRequest setSortDescriptors:sortDescriptors];
-    [fetchRequest setFetchBatchSize:100];
-    [fetchRequest setFetchLimit:max];
     
-    NSFetchedResultsController *fetchedResultsController =
-    [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
-                                        managedObjectContext:[self managedObjectContext]
-                                          sectionNameKeyPath:nil
-                                                   cacheName:nil];
-    NSError *error;
-    BOOL fetchResult = [fetchedResultsController performFetch:&error];
-    
-    // get result
-    if (fetchResult) {
-        return fetchedResultsController.fetchedObjects;
+    return [self fetchResultsByPredicate:predicate sortDescriptors:sortDescriptors maxCount:max];
+}
+
+#pragma mark - 多條件
+
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values
+{
+    return [self findBy:columnNames values:values limit:0];
+}
+
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values limit:(NSUInteger)max
+{
+    return [self findBy:columnNames values:values limit:max sortBy:nil ascending:NO];
+}
+
++ (NSArray *)findBy:(NSArray *)columnNames values:(NSArray *)values limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending
+{
+    NSString *predicateStr = @"";
+    for (NSInteger i=0; i<columnNames.count; i++) {
+        NSString *columnName = columnNames[i];
+        id value = values[i];
+        NSString *and;
+        if (i != 0) {
+            and = @" AND ";
+        }
+        predicateStr = [NSString stringWithFormat:@"%@%@self.%@ == %@", predicateStr, and, columnName, value];
     }
     
-    return nil;
+    // *** predicate
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"%@", predicateStr];
+    
+    // *** sort conditions
+    NSMutableArray *sortDescriptors = [[NSMutableArray alloc] init];
+    if (sortColumns) {
+        for (NSString *sortColumn in sortColumns) {
+            NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:sortColumn ascending:ascending];
+            [sortDescriptors addObject:sort];
+            NSLog(@"condition: %@", sortColumn);
+        }
+    } else {
+        NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"createdAt" ascending:ascending];
+        [sortDescriptors addObject:sort];
+    }
+    
+    return [self fetchResultsByPredicate:predicate sortDescriptors:sortDescriptors maxCount:max];
 }
+
+#pragma mark - 需要自己在外部產生 predicate 來搜尋
+
++ (NSArray *)findByPredicate:(NSPredicate *)predicate
+{
+    return [self findByPredicate:predicate limit:0];
+}
+
++ (NSArray *)findByPredicate:(NSPredicate *)predicate limit:(NSUInteger)max
+{
+    return [self findByPredicate:predicate limit:0 sortBy:nil ascending:NO];
+}
+
++ (NSArray *)findByPredicate:(NSPredicate *)predicate limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending
+{
+    NSMutableArray *sortDescriptors = [[NSMutableArray alloc] init];
+    if (sortColumns) {
+        for (NSString *sortColumn in sortColumns) {
+            NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:sortColumn ascending:ascending];
+            [sortDescriptors addObject:sort];
+            NSLog(@"condition: %@", sortColumn);
+        }
+    } else {
+        NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"createdAt" ascending:ascending];
+        [sortDescriptors addObject:sort];
+    }
+    
+    return [self fetchResultsByPredicate:predicate sortDescriptors:sortDescriptors maxCount:max];
+}
+
+//+ (NSArray *)findBy:(NSString *)columnName value:(id)value limit:(NSUInteger)max sortBy:(NSArray *)sortColumns ascending:(BOOL)ascending
+//{
+//    // *** create fetchRequest
+//    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+//    NSEntityDescription *entity = [NSEntityDescription
+//                                   entityForName:SELF_CLASS_STRING
+//                                   inManagedObjectContext:[self managedObjectContext]];
+//    [fetchRequest setEntity:entity];
+//    
+//    // *** predicate
+//    if (columnName) {
+//        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self.%@ == %@", columnName, value];
+//        [fetchRequest setPredicate:predicate];
+//        NSLog(@"%@", predicate);
+//    }
+//    
+//    // *** sort conditions
+//    NSMutableArray *sortDescriptors = [[NSMutableArray alloc] init];
+//    if (sortColumns) {
+//        for (NSString *sortColumn in sortColumns) {
+//            NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:sortColumn ascending:ascending];
+//            [sortDescriptors addObject:sort];
+//            NSLog(@"condition: %@", sortColumn);
+//        }
+//    } else {
+//        NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"createdAt" ascending:ascending];
+//        [sortDescriptors addObject:sort];
+//    }
+//    [fetchRequest setSortDescriptors:sortDescriptors];
+//    [fetchRequest setFetchBatchSize:100];
+//    [fetchRequest setFetchLimit:max];
+//    
+//    NSFetchedResultsController *fetchedResultsController =
+//    [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+//                                        managedObjectContext:[self managedObjectContext]
+//                                          sectionNameKeyPath:nil
+//                                                   cacheName:nil];
+//    NSError *error;
+//    BOOL fetchResult = [fetchedResultsController performFetch:&error];
+//    
+//    // get result
+//    if (fetchResult) {
+//        return fetchedResultsController.fetchedObjects;
+//    }
+//    
+//    return nil;
+//}
 
 #pragma mark - private method
 
@@ -218,6 +315,39 @@
     } else {
         NSLog(@"\n*****************\n***** ERROR *****\n*****************\nmanaged object 沒有 updatedAt 參數");
     }
+}
+
++ (NSArray *)fetchResultsByPredicate:(NSPredicate *)predicate sortDescriptors:(NSArray *)sortDescriptors maxCount:(NSUInteger)maxCount
+{
+    // *** create fetchRequest
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription
+                                   entityForName:SELF_CLASS_STRING
+                                   inManagedObjectContext:[self managedObjectContext]];
+    [fetchRequest setEntity:entity];
+    
+    // *** predicate
+    [fetchRequest setPredicate:predicate];
+    
+    // *** sort conditions
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    [fetchRequest setFetchBatchSize:100];
+    [fetchRequest setFetchLimit:maxCount];
+    
+    NSFetchedResultsController *fetchedResultsController =
+    [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                        managedObjectContext:[self managedObjectContext]
+                                          sectionNameKeyPath:nil
+                                                   cacheName:nil];
+    NSError *error;
+    BOOL fetchResult = [fetchedResultsController performFetch:&error];
+    
+    // get result
+    if (fetchResult) {
+        return fetchedResultsController.fetchedObjects;
+    }
+    
+    return nil;
 }
 
 @end
